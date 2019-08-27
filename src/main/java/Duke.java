5df9bb6794232c9fa90c21d0a2cd1f6d509630dd
VivianLee <e0325814@u.nus.edu>
2019-08-27 20:09:26 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Duke {
     /**
@@ -13,13 +15,25 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        // Level 1: Greet, Echo, Exit
+        // Level 2: Add, List
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         Scanner scan = new Scanner(System.in);
-        String input = "";
+        String input = scan.nextLine();
+        String[] tasks = new String[100];
+        int x = 0;
         while (!input.equals("bye")) {
+            if (input.equals("list") && tasks.length != 0) {
+                int count = 0;
+                for (int i = 0; i < x; i++) {
+                    System.out.println(++count + ". " + tasks[i]);
+                }
+            }
+            else {
+                tasks[x] = input;
+                System.out.println("added: " + input);
+                x = x + 1;
+            }
             input = scan.nextLine();
-            System.out.println(input);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
