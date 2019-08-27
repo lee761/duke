e770e6e57ac4d7fac0be5c4566ebea54fa395563
VivianLee <e0325814@u.nus.edu>
2019-08-27 10:55:30 +0800

    Level 1 - Greet, Echo, Repeat

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     /**
      * To print the word Duke.
@@ -9,5 +12,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Level 1: Greet, Echo, Exit
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+        Scanner scan = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("bye")) {
+            input = scan.nextLine();
+            System.out.println(input);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
