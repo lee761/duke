8bc4cd6682eb6265b2161470d14ab6592b98a76f
VivianLee <e0325814@u.nus.edu>
2019-09-04 04:26:22 +0800

    Level 6: Delete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -18,7 +20,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        // Level 8: Dates and Times
+        // Level 6: Delete
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         System.out.println("__________________________________________");
         Scanner scan = new Scanner(System.in);
@@ -60,6 +62,23 @@ public class Duke {
                     } catch (DukeException e) {
                         System.out.println(e);
                     }
+                } else if (input.contains("delete")) {
+                    String[] taskNum = input.split(Pattern.quote(" "));
+                    int num = Integer.parseInt(taskNum[1]) - 1;
+                    Task t = tasks[num];
+                    type = t.taskType();
+                    add = t.others();
+                    ArrayList<Task> list = new ArrayList<>();
+                    for (int k = 0; k < x + y; k++) {
+                        list.add(tasks[k]);
+                    }
+                    list.remove(num);
+                    for (int i = 0; i < list.size(); i++) {
+                        tasks[i] = list.get(i);
+                    }
+                    x = x - 1;
+                    System.out.println("Noted. I've removed this task: \n" + type + "[" + t.getStatusIcon() + "] " + t.description + add + "\nNow you have " + (x + y) + " tasks in the list.");
+                    Save.saveToFile(tasks, (x + y));
                 } else {
                     if (input.contains("todo")) {
                         try {
@@ -68,7 +87,7 @@ public class Duke {
                             ToDo newTodo = new ToDo(taskOne[1]);
                             tasks[x+y] = newTodo;
                             x = x + 1;
-                            System.out.println("Got it. I've added this task: \n" + newTodo.taskType() + "[" + newTodo.getStatusIcon() + "] " + newTodo.description + "\nNow you have " + (x+y) + " tasks in the list.");
+                            System.out.println("Got it. I've added this task: \n" + newTodo.taskType() + "[" + newTodo.getStatusIcon() + "] " + newTodo.description + "\nNow you have " + (x + y) + " tasks in the list.");
                             Save.saveToFile(tasks, (x + y));
                         } catch (DukeException e) {
                             System.out.println(e);
@@ -102,7 +121,7 @@ public class Duke {
                             Event newEv = new Event(taskThree[0], taskThree[1]);
                             tasks[x+y] = newEv;
                             x = x + 1;
-                            System.out.println("Got it. I've added this task: \n" + newEv.taskType() + "[" + newEv.getStatusIcon() + "] " + newEv.description + " (at: " + newEv.at + ")" + "\nNow you have " + (x+y) + " tasks in the list.");
+                            System.out.println("Got it. I've added this task: \n" + newEv.taskType() + "[" + newEv.getStatusIcon() + "] " + newEv.description + " (at: " + newEv.at + ")" + "\nNow you have " + (x + y) + " tasks in the list.");
                             Save.saveToFile(tasks, (x + y));
                         } catch (DukeException e) {
                             System.out.println(e);
@@ -120,42 +139,6 @@ public class Duke {
         System.out.println("__________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
     }
-//    // To catch invalid inputs
-//    private static void checkInput(String in) throws DukeException {
-//        String[] inputs = {"todo", "deadline", "event", "list", "done"};
-//        boolean checker = false;
-//        for (int i = 0; i < inputs.length; i++) {
-//            if (in.contains(inputs[i])) {
-//                checker = true;
-//            }
-//        }
-//        if (in.isEmpty()) {
-//            throw new DukeException("Task cannot be empty. Please type a task.");
-//        }
-//        else if (!checker) {
-//            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-//        }
-//    }
-//
-//    // To catch invalid "done" task
-//    private static void checkTask(Task[] Ta, int x, int size) throws DukeException {
-//        if (x >= size || x < 0) {
-//            throw new DukeException("☹ OOPS!!! There is no such task to be done. Please enter a valid task number.");
-//        }
-//        else {
-//            Task checkTa = Ta[x];
-//            if (checkTa.status()) {
-//                throw new DukeException("This task has been done. Please enter an incomplete task.");
-//            }
-//        }
-//    }
-//
-//    // To catch invalid task
-//    private static void checkCmd(String[] t) throws DukeException {
-//        if (t.length < 2 || t[1].isEmpty()) {
-//            throw new DukeException("☹ OOPS!!! The description of a task cannot be empty.");
-//        }
-//    }
 }
 
 
